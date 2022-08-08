d3d95f86ec0d4215dde21e0259fc6ab4da74d720
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-08-08 22:23:03 +0000

    Adding a Second Route and Template

--- express_server.js
+++ express_server.js
@@ -14,13 +14,19 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+/// Route for urls_index
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-app.get("/hello", (req, res) => {
-  res.status(404);
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+/// Route for urls_show
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
+
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
