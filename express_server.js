44e267c1fd18d13e19da312a12642785b79624f3
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-08-09 01:40:10 +0000

    Creating a Submission For

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs"); // setting up ejs as the view engine.
+app.use(express.urlencoded({ extended: true }));
+
+function generateRandomString() {}
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -19,6 +22,11 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+/// GET Route to Show the Form
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 /// Route for urls_show
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
@@ -27,6 +35,12 @@ app.get("/urls/:id", (req, res) => {
   };
 
   res.render("urls_show", templateVars);
+});
+
+/// Post request
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok'
 });
 
 app.listen(PORT, () => {
