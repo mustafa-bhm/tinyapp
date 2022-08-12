d5eed9b29f3f4e056b3cae022275bd325834384d
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-08-12 03:38:51 +0000

    testing folder

---
+++ test/helpersTest.js
@@ -0,0 +1,29 @@
+const { assert } = require("chai");
+
+const { getUserByEmail } = require("../helpers.js");
+
+const testUsers = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+describe("getUserByEmail", function () {
+  it("should return a user with valid email", function () {
+    const user = getUserByEmail("user@example.com", testUsers);
+    const expectedUserID = "userRandomID";
+    assert.equal(user, expectedUserID);
+  });
+  it("it should return undefined for a nond existing email", function () {
+    const user = getUserByEmail("user222@example.com", testUsers);
+    const expectedResult = undefined;
+    assert.equal(user, expectedResult);
+  });
+});
